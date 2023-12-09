e3079e1f6f4054230217ea08a95edbbc198270d3
Emmanuel <emmanuelabraham999@gmail.com>
2023-12-09 13:56:39 +0530

    Initial commit

---
+++ app/src/main/java/com/company/sneakership/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.company.sneakership.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.company.sneakership.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
