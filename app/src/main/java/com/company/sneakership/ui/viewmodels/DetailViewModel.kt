85b82930d56eaa678661af20ea940b735c6b45d5
Emmanuel <emmanuelabraham999@gmail.com>
2023-12-11 07:49:55 +0530

    Comments removed

--- app/src/main/java/com/company/sneakership/ui/viewmodels/DetailViewModel.kt
+++ app/src/main/java/com/company/sneakership/ui/viewmodels/DetailViewModel.kt
@@ -1,7 +1,5 @@
 package com.company.sneakership.ui.viewmodels
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DetailViewModel : ViewModel() {
